f0d4cffc43229ebc6c8ab77733ca142925133947
Eugene Lin <eugenelin89@gmail.com>
2018-11-08 23:05:12 -0800

    use dataframe rather than dictionary

--- model.R
+++ model.R
@@ -1,22 +1,43 @@
 library(dplyr)
+library(dict)
 
 # input is ngram dataframe
 learn_from_ngrams <- function(ngram, model = NULL){
-  if(is.null(model)){
-    model <- dict()
+  
+  
+  model_df <- data.frame(matrix(ncol = 2, nrow = nrow(ngram)))
+  colnames(model_df) <- c("key", "value")
+  n = 0
+  if(!is.null(model)){
+    n = nrow(model) # number of entries
+    model_df <- rbind(model, model_df)
   }
+  
   for(i in 1: nrow(ngram)){
+    if(i%%100 == 0){
+      print(i) # for visual inspection
+    }
     rw <- ngram[i,]
     if(is.null(rw$word3)){ # 2-gram
       
     }else{ # 3-gram
       key <- paste(rw$word1, rw$word2, sep = " ") 
-      n <- rw$n
-      val <- rep(rw$word3, n)
-      model[[key]] <- c(model$get(key,character()), val)
+      val <- paste(rep(rw$word3, rw$n), collapse = " ")
+      
+      y <- which(model_df$key==key)
+      # model[[key]] <- c(model$get(key,character()), val)
+      if(length(y) == 0){
+        # Add new entry
+        n = n + 1
+        model_df[n,] <- c(key, val)
+      }else{
+        # Update existing entry
+        cur_val <- model_df[y,"value"]
+        model_df[y,"value"] <- paste(c(cur_val, val), collapse = " ")
+      }
     }
   }
-  model
+  model_df[complete.cases(model_df),]
 }
 
 tfunc <- function(foo = NULL){
